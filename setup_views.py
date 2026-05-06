95b719d673626974976e2da4a19289eb9be8282c
Awanit Raj <awanitraj121@gmail.com>
2026-05-06 22:22:05 +0530

    Fix DATABASE_URL assignment in setup_views.py

--- setup_views.py
+++ setup_views.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, text
 
 
-DATABASE_URL = 'postgresql://awanitraj@localhost:5432/retail_db'
+DATABASE_URL 
 engine = create_engine(DATABASE_URL)
 
 create_view_sql = """
